4ff42d12dca3597f6749306b1a62f5aaad211bc8
Luke <120085361+LukeJS0326@users.noreply.github.com>
2023-02-15 11:55:31 +0900

    Part 3: Create "QuestionList" view using APIView Class
    Create "QuestionList" view using APIView Class and Implement GET/POST Methods.
    And define the mapping between URLs and views.

--- mysite/polls_api/urls.py
+++ mysite/polls_api/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('question/', question_list, name='question-list'),
-    path('question/<int:id>/', question_detail, name='question-detail'),
+    #path('question/', question_list, name='question-list'),
+    #path('question/<int:id>/', question_detail, name='question-detail'),
+
+    path('question/', QuestionList.as_view(), name='question-list'),
 
 
 ]
--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -4,41 +4,91 @@ from polls.models import Question
 from polls_api.serializers import QuestionSerializer
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.views import APIView
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-def question_list(request):
-    if request.method == 'GET':
+"""
+Class-based Views
+"""
+
+class QuestionList(APIView):
+
+    def get(self, request):
         questions = Question.objects.all()
         serializer = QuestionSerializer(questions,many = True)
         return Response(serializer.data)
-    
-    if request.method == 'POST':
+
+
+    def post(self, request):   
         serializer = QuestionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors) 
 
-@api_view(['GET','PUT','DELETE'])
-def question_detail(request,id):
-    if request.method == 'GET':
-        question = Question.objects.get(pk=id)
-        serializer = QuestionSerializer(question)
-        return Response(serializer.data)
 
-    if request.method == 'PUT':
-        question = Question.objects.get(pk=id)
-        serializer = QuestionSerializer(question, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+# class QuestionDetailView(APIView):
+
+#     def get(self, request, id):
+#         question = models.Question.objects.get(pk=id)
+#         serializer = serializers.QuestionSerializer(question)
+#         return Response(serializer.data)
+
+
+#     def put(self, request, id):
+#         question = models.Question.objects.get(pk=id)
+#         serializer = serializers.QuestionSerializer(question, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+
     
-    if request.method == 'DELETE':
-        question = Question.objects.get(pk=id)
-        question.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, id):
+#         question = models.Question.objects.get(pk=id)
+#         question.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+"""
+Function-based Views
+"""
+
+# @api_view(['GET', 'POST'])
+# def question_list(request):
+#     if request.method == 'GET':
+#         questions = Question.objects.all()
+#         serializer = QuestionSerializer(questions,many = True)
+#         return Response(serializer.data)
+    
+#     if request.method == 'POST':
+#         serializer = QuestionSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+
+# @api_view(['GET','PUT','DELETE'])
+# def question_detail(request,id):
+#     if request.method == 'GET':
+#         question = Question.objects.get(pk=id)
+#         serializer = QuestionSerializer(question)
+#         return Response(serializer.data)
+
+#     if request.method == 'PUT':
+#         question = Question.objects.get(pk=id)
+#         serializer = QuestionSerializer(question, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+    
+#     if request.method == 'DELETE':
+#         question = Question.objects.get(pk=id)
+#         question.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
