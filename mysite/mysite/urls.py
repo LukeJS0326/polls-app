9267510d270bffcecdbc4c03ee10e532fbb5fe2d
Luke <120085361+LukeJS0326@users.noreply.github.com>
2023-02-15 10:17:53 +0900

    Create "polls_api" app
    Create "polls_api" app and Set up basic settings in project

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('rest/', include('polls_api.urls')),
    
 ]
