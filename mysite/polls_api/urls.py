cbc4d8bddb2faa757e4d9a47d91f9616f954640f
Tod <tod@grepp.co>
2023-02-15 07:31:27 -0800

    GET구현

--- mysite/polls_api/urls.py
+++ mysite/polls_api/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, include
+from django.urls import path
 from .views import *
 
 urlpatterns = [
-
-
+    path('question/', question_list, name='question-list'),
 ]
--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
 # Create your views here.
+from polls.models import Question
+from polls_api.serializers import QuestionSerializer
+from rest_framework.response import Response
+
+@api_view()
+def question_list(request):
+    questions = Question.objects.all()
+    serializer = QuestionSerializer(questions, many = True)
+    return Response(serializer.data)
