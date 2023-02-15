41c7c0ce1005e40e0c582b58664b84e8c37ed2b2
Luke <120085361+LukeJS0326@users.noreply.github.com>
2023-02-15 10:46:15 +0900

    Part 1 : Define Create() and Update() methods in Serializer

--- mysite/polls_api/serializers.py
+++ mysite/polls_api/serializers.py
@@ -6,3 +6,17 @@ class QuestionSerializer(serializers.Serializer):
    id = serializers.IntegerField(read_only=True)
    question_text = serializers.CharField(max_length=200)
    pub_date = serializers.DateTimeField(read_only=True)
+
+   def create(self, validated_data):
+        """
+        Create and return a new `Question` instance, given the validated data.
+        """
+        return Question.objects.create(**validated_data)
+    
+   def update(self, instance, validated_data):
+       """
+       Update and return an existing `Question` instance, given the validated data.
+       """
+       instance.question_text = validated_data.get('question_text', instance.question_text)
+       instance.save()
+       return instance
