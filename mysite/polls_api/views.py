3270a0b10de7f121f4e9a44de1cf8dbd1fc4fe59
Luke <120085361+LukeJS0326@users.noreply.github.com>
2023-02-15 11:46:03 +0900

    Part2 : Implement DELETE Method
    Implement DELETE method on "question_detail" view.
    And import "status" module to return HTTP Status Code.

--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from polls.models import Question
 from polls_api.serializers import QuestionSerializer
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
 
@@ -21,7 +22,7 @@ def question_list(request):
         else:
             return Response(serializer.errors)
 
-@api_view(['GET','PUT'])
+@api_view(['GET','PUT','DELETE'])
 def question_detail(request,id):
     if request.method == 'GET':
         question = Question.objects.get(pk=id)
@@ -36,3 +37,8 @@ def question_detail(request,id):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+    
+    if request.method == 'DELETE':
+        question = Question.objects.get(pk=id)
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
