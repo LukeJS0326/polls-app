0f559a122e005a75571e3cf1e7f5676653f9cdaf
Luke <120085361+LukeJS0326@users.noreply.github.com>
2023-02-15 10:30:59 +0900

    Part 1 : Creating QuestionSerializer class
    Create a Serializer for Question model and Defines the fields that get serialized/deserialized

--- mysite/polls_api/serializers.py
+++ mysite/polls_api/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
 from polls.models import Question, Choice
+
+class QuestionSerializer(serializers.Serializer):
+   "The fields that get serialized/deserialized"
+   id = serializers.IntegerField(read_only=True)
+   question_text = serializers.CharField(max_length=200)
+   pub_date = serializers.DateTimeField(read_only=True)
