02111979fc1a334290e8df36fe063f3d62601fbb
Luke <120085361+LukeJS0326@users.noreply.github.com>
2023-02-15 10:50:26 +0900

    Part 1 : Using ModelSerializer
    Refactor QuestionSerializer using ModelSerializer

--- mysite/polls_api/serializers.py
+++ mysite/polls_api/serializers.py
@@ -1,22 +1,27 @@
 from rest_framework import serializers
 from polls.models import Question, Choice
 
-class QuestionSerializer(serializers.Serializer):
-   "The fields that get serialized/deserialized"
-   id = serializers.IntegerField(read_only=True)
-   question_text = serializers.CharField(max_length=200)
-   pub_date = serializers.DateTimeField(read_only=True)
+# class QuestionSerializer(serializers.Serializer):
+#    "The fields that get serialized/deserialized"
+#    id = serializers.IntegerField(read_only=True)
+#    question_text = serializers.CharField(max_length=200)
+#    pub_date = serializers.DateTimeField(read_only=True)
 
-   def create(self, validated_data):
-        """
-        Create and return a new `Question` instance, given the validated data.
-        """
-        return Question.objects.create(**validated_data)
+#    def create(self, validated_data):
+#         """
+#         Create and return a new `Question` instance, given the validated data.
+#         """
+#         return Question.objects.create(**validated_data)
     
-   def update(self, instance, validated_data):
-       """
-       Update and return an existing `Question` instance, given the validated data.
-       """
-       instance.question_text = validated_data.get('question_text', instance.question_text)
-       instance.save()
-       return instance
+#    def update(self, instance, validated_data):
+#        """
+#        Update and return an existing `Question` instance, given the validated data.
+#        """
+#        instance.question_text = validated_data.get('question_text', instance.question_text)
+#        instance.save()
+#        return instance
+
+class QuestionSerializer(serializers.ModelSerializer):
+   class Meta:
+       model = Question
+       fields = ['id', 'question_text', 'pub_date']
