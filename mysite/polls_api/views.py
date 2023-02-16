3832e3d849b6e1d122acc6d22aadd5eddb409aad
Luke <120085361+LukeJS0326@users.noreply.github.com>
2023-02-16 10:56:26 +0900

    Adding endpoints for User models

--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from polls.models import Question
-from polls_api.serializers import QuestionSerializer
+from polls_api.serializers import QuestionSerializer,UserSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
@@ -16,6 +17,14 @@ class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 
