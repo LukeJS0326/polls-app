b269e3cc267bb5d3474d6a7b250a37b942eeb7c8
Luke <120085361+LukeJS0326@users.noreply.github.com>
2023-02-16 09:55:45 +0900

    QuestionDetail using generic class-based views
    Mixins 클래스를 활용하여 작성하였던 QuestionDetail을 다시 Generic class로 리팩토링해주었습니다.

--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -12,21 +12,9 @@ class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
-class QuestionDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
 
 
