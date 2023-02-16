5cbfb34fbb452665bf876b7cc8549ebd4ef7b068
Luke <120085361+LukeJS0326@users.noreply.github.com>
2023-02-16 10:50:56 +0900

    Creating UserSerializer

--- mysite/polls_api/serializers.py
+++ mysite/polls_api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from polls.models import Question, Choice
-
+from django.contrib.auth.models import User
 # class QuestionSerializer(serializers.Serializer):
 #    "The fields that get serialized/deserialized"
 #    id = serializers.IntegerField(read_only=True)
@@ -25,3 +25,10 @@ class QuestionSerializer(serializers.ModelSerializer):
    class Meta:
        model = Question
        fields = ['id', 'question_text', 'pub_date']
+
+class UserSerializer(serializers.ModelSerializer):
+    questions = serializers.PrimaryKeyRelatedField(many=True, queryset=Question.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'questions']
