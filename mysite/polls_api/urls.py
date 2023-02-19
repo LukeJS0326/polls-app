e11789f0c81a951ed56defea1731d92351226390
Tod <tod@grepp.co>
2023-02-19 12:55:10 -0800

    UserSerializer구현

--- mysite/polls_api/urls.py
+++ mysite/polls_api/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
     path('question/', QuestionList.as_view(), name='question-list'),
     path('question/<int:pk>/', QuestionDetail.as_view(), name='question-detail'),
+    path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view()),
 ]
