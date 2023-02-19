a9bb6e57481fe15fa81733d0aa60147499513f24
Tod <tod@grepp.co>
2023-02-19 15:08:19 -0800

    DRF로 User만드는 기능

--- mysite/polls_api/serializers.py
+++ mysite/polls_api/serializers.py
@@ -13,3 +13,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'questions']
+        
+        
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
