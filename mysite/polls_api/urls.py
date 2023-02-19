45c67469288328d3a3588d96583d0a65f4dd5b8b
Tod <tod@grepp.co>
2023-02-19 14:59:27 -0800

    Revert "사용자 생성 화면 만들기"
    This reverts commit 51182075775a04ca511304b933239c19aeb9e565.

--- mysite/polls_api/urls.py
+++ mysite/polls_api/urls.py
@@ -4,8 +4,6 @@ from .views import *
 urlpatterns = [
     path('question/', QuestionList.as_view(), name='question-list'),
     path('question/<int:pk>/', QuestionDetail.as_view(), name='question-detail'),
-    path('users/', UserList.as_view(), name='user-list'),
+    path('users/', UserList.as_view()),
     path('users/<int:pk>/', UserDetail.as_view()),
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('api-auth/', include('rest_framework.urls')),
 ]
