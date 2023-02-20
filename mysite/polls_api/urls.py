b92d286f4ffd665f1f9f6a6169faad30324041c7
Tod <tod@grepp.co>
2023-02-20 09:39:42 -0800

    .

--- mysite/polls_api/urls.py
+++ mysite/polls_api/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('question/', QuestionList.as_view(), name='question-list'),
     path('question/<int:pk>/', QuestionDetail.as_view(), name='question-detail'),
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view(), name='user-list'),
+    path('users/', UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetail.as_view()),
     path('register/', RegisterUser.as_view()),
 ]
