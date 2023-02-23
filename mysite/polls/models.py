03240fbb76ddb997405eee107fe6f9ebcb41287c
Tod <tod@grepp.co>
2023-02-22 18:36:16 -0800

    votes만들기

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -23,3 +23,13 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Vote(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['question', 'voter'], name='unique_voter_for_questions')
+        ]
