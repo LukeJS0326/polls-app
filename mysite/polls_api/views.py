780450eab18e58e4e2edf210cd0e22e21d347470
Tod <tod@grepp.co>
2023-02-23 12:08:41 -0800

    투표기능 구현

--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -1,8 +1,23 @@
-from polls.models import Question
-from polls_api.serializers import QuestionSerializer, UserSerializer, RegisterSerialzier
+from polls.models import Question, Vote
+from polls_api.serializers import QuestionSerializer, UserSerializer, RegisterSerialzier, VoteSerializer
 from rest_framework import generics, permissions
 from django.contrib.auth.models import User
-from .permissions import IsOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnly, IsVoter
+
+class VoteList(generics.ListCreateAPIView):
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self, *args, **kwargs):
+        return Vote.objects.filter(voter=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(voter=self.request.user)
+    
+class VoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated, IsVoter]
 
 class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
